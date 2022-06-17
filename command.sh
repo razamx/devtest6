beeed960b43bd7d33e06e3ebc22c1d25b1a4c839
razamx <105820617+razamx@users.noreply.github.com>
2022-06-17 09:35:48 -0500

    commit

--- command.sh
+++ command.sh
@@ -69,7 +69,7 @@ mv ${dirBuildRoot}/build ${dirBuildRoot}/build-host
 #rm -rf ${dirBuildRoot}/build/tcc_tools*.tar.gz
 #tar --owner=root --group=root --exclude='usr/tests' -cvzf ${dirBuildRoot}/build/tcc_tools_target_2022.1.0.tar.gz usr
 
-# Part three: add efi module (by way of edk2 project).
+#  Part three: add efi module (by way of edk2 project).
 set -ex
 mkdir -p /opt
 cd /opt
